9732503032c0a9bd1178555c22bd2d52ffe56939
Ashish Mishra <ashish@gooru.org>
2015-12-25 13:50:40 +0530

    Initial repo creation

---
+++ src/main/java/org/gooru/nucleus/handlers/assessment/bootstrap/AssessmentVerticle.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.assessment.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class AssessmentVerticle extends AbstractVerticle {
+}
