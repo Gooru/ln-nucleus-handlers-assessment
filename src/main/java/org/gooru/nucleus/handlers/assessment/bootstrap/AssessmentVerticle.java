9db48b8ff5d165ba9127e5cf60c57b8cc01ceaf4
Ashish Mishra <ashish@gooru.org>
2016-01-06 21:21:40 +0530

    Provide dummy holders for handlers. Modify the machinery to work off the MessageResponse instead of JsonObject

--- src/main/java/org/gooru/nucleus/handlers/assessment/bootstrap/AssessmentVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/assessment/bootstrap/AssessmentVerticle.java
@@ -12,6 +12,7 @@ import org.gooru.nucleus.handlers.assessment.bootstrap.startup.Initializers;
 import org.gooru.nucleus.handlers.assessment.constants.MessageConstants;
 import org.gooru.nucleus.handlers.assessment.constants.MessagebusEndpoints;
 import org.gooru.nucleus.handlers.assessment.processors.ProcessorBuilder;
+import org.gooru.nucleus.handlers.assessment.processors.responses.MessageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,14 +44,13 @@ public class AssessmentVerticle extends AbstractVerticle {
       LOGGER.debug("Received message: " + message.body());
 
       vertx.executeBlocking(future -> {
-        JsonObject result = new ProcessorBuilder(message).build().process();
+        MessageResponse result = new ProcessorBuilder(message).build().process();
         future.complete(result);
       }, res -> {
-        JsonObject result = (JsonObject) res.result();
-        DeliveryOptions options = new DeliveryOptions().addHeader(MessageConstants.MSG_OP_STATUS, result.getString(MessageConstants.MSG_OP_STATUS));
-        message.reply(result.getJsonObject(MessageConstants.RESP_CONTAINER_MBUS), options);
+        MessageResponse result = (MessageResponse) res.result();
+        message.reply(result.reply(), result.deliveryOptions());
 
-        JsonObject eventData = result.getJsonObject(MessageConstants.RESP_CONTAINER_EVENT);
+        JsonObject eventData = result.event();
         if (eventData != null) {
           eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
         }
--- src/main/java/org/gooru/nucleus/handlers/assessment/processors/MessageProcessor.java
+++ src/main/java/org/gooru/nucleus/handlers/assessment/processors/MessageProcessor.java
@@ -5,6 +5,8 @@ import io.vertx.core.json.JsonObject;
 import org.gooru.nucleus.handlers.assessment.constants.MessageConstants;
 import org.gooru.nucleus.handlers.assessment.processors.exceptions.InvalidRequestException;
 import org.gooru.nucleus.handlers.assessment.processors.exceptions.InvalidUserException;
+import org.gooru.nucleus.handlers.assessment.processors.responses.MessageResponse;
+import org.gooru.nucleus.handlers.assessment.processors.responses.MessageResponseFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,61 +23,64 @@ class MessageProcessor implements Processor {
   }
   
   @Override
-  public JsonObject process() {
-    JsonObject result;
+  public MessageResponse process() {
+    MessageResponse result = null;
     try {
       if (message == null || !(message.body() instanceof JsonObject)) {
         LOGGER.error("Invalid message received, either null or body of message is not JsonObject ");
-        throw new InvalidRequestException();
+        return MessageResponseFactory.createInvalidRequestResponse();
       }
       
       final String msgOp = message.headers().get(MessageConstants.MSG_HEADER_OP);
       userId = ((JsonObject)message.body()).getString(MessageConstants.MSG_USER_ID);
       if (userId == null) {
         LOGGER.error("Invalid user id passed. Not authorized.");
-        throw new InvalidUserException();
+        return MessageResponseFactory.createForbiddenResponse();
       }
       prefs = ((JsonObject)message.body()).getJsonObject(MessageConstants.MSG_KEY_PREFS);
       request = ((JsonObject)message.body()).getJsonObject(MessageConstants.MSG_HTTP_BODY);
       switch (msgOp) {
-      case MessageConstants.MSG_OP_ASSESS_CREATE:
+      case MessageConstants.MSG_OP_ASSESSMENT_CREATE:
         result = processAssessmentCreate();
         break;
-      case MessageConstants.MSG_OP_ASSESS_GET:
+      case MessageConstants.MSG_OP_ASSESSMENT_GET:
         result = processAssessmentGet();
         break;
-      case MessageConstants.MSG_OP_ASSESS_UPDATE:
+      case MessageConstants.MSG_OP_ASSESSMENT_UPDATE:
         result = processAssessmentUpdate();
+        break;
+      case MessageConstants.MSG_OP_ASSESSMENT_DELETE:
+      case MessageConstants.MSG_OP_ASSESSMENT_ADD_QUESTION:
+      case MessageConstants.MSG_OP_ASSESSMENT_COPY_QUESTION:
+      case MessageConstants.MSG_OP_ASSESSMENT_REMOVE_QUESTION:
+      case MessageConstants.MSG_OP_ASSESSMENT_COLLABORATOR_GET:
+      case MessageConstants.MSG_OP_ASSESSMENT_COLLABORATOR_UPDATE:
         break;
       default:
         LOGGER.error("Invalid operation type passed in, not able to handle");
-        throw new InvalidRequestException();
+        return MessageResponseFactory.createInvalidRequestResponse();
       }
       return result;
-    } catch (InvalidRequestException e) {
-      // TODO: handle exception
-    } catch (InvalidUserException e) {
-      // TODO: handle exception
+    } catch (Throwable e) {
+      return MessageResponseFactory.createInternalErrorResponse();
     }
-
-    return null;
   }
 
-  private JsonObject processAssessmentUpdate() {
+  private MessageResponse processAssessmentUpdate() {
     // TODO Auto-generated method stub
     String assessmentId = message.headers().get(MessageConstants.ASSESSMENT_ID);
     
     return null;    
   }
 
-  private JsonObject processAssessmentGet() {
+  private MessageResponse processAssessmentGet() {
     // TODO Auto-generated method stub
     String assessmentId = message.headers().get(MessageConstants.ASSESSMENT_ID);
     
     return null;
   }
 
-  private JsonObject processAssessmentCreate() {
+  private MessageResponse processAssessmentCreate() {
     // TODO Auto-generated method stub
     
     return null;    
