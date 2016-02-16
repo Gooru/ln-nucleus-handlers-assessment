d678c8480f1490ee4f6b02a1dc062f3cf5136b0a
am-gooru <ashish@gooru.org>
2016-02-16 07:21:32 +0530

    Use the session token if present to be sent along with the event payload

--- src/main/java/org/gooru/nucleus/handlers/assessment/bootstrap/AssessmentVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/assessment/bootstrap/AssessmentVerticle.java
@@ -8,6 +8,7 @@ import org.gooru.nucleus.handlers.assessment.bootstrap.shutdown.Finalizer;
 import org.gooru.nucleus.handlers.assessment.bootstrap.shutdown.Finalizers;
 import org.gooru.nucleus.handlers.assessment.bootstrap.startup.Initializer;
 import org.gooru.nucleus.handlers.assessment.bootstrap.startup.Initializers;
+import org.gooru.nucleus.handlers.assessment.constants.MessageConstants;
 import org.gooru.nucleus.handlers.assessment.constants.MessagebusEndpoints;
 import org.gooru.nucleus.handlers.assessment.processors.ProcessorBuilder;
 import org.gooru.nucleus.handlers.assessment.processors.responses.MessageResponse;
@@ -50,9 +51,14 @@ public class AssessmentVerticle extends AbstractVerticle {
 
         JsonObject eventData = result.event();
         if (eventData != null) {
+          String sessionToken = ((JsonObject) message.body()).getString(MessageConstants.MSG_HEADER_TOKEN);
+          if (sessionToken != null && !sessionToken.isEmpty()) {
+            eventData.put(MessageConstants.MSG_HEADER_TOKEN, sessionToken);
+          } else {
+            LOGGER.warn("Invalid session token received");
+          }
           eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
         }
-
       });
 
 
