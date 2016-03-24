c5104fc78451fc743e9b5d85a05a3805b1cffd47
am-gooru <ashish@gooru.org>
2016-03-24 13:33:18 +0530

    Make the audience field as json array instead of json

--- src/main/java/org/gooru/nucleus/handlers/assessment/processors/repositories/activejdbc/entities/AJEntityAssessment.java
+++ src/main/java/org/gooru/nucleus/handlers/assessment/processors/repositories/activejdbc/entities/AJEntityAssessment.java
@@ -107,7 +107,7 @@ public class AJEntityAssessment extends Model {
     validatorMap.put(TITLE, (value) -> FieldValidator.validateString(value, 5000));
     validatorMap.put(THUMBNAIL, (value) -> FieldValidator.validateStringIfPresent(value, 2000));
     validatorMap.put(LEARNING_OBJECTIVE, (value) -> FieldValidator.validateStringIfPresent(value, 20000));
-    validatorMap.put(AUDIENCE, FieldValidator::validateJsonIfPresent);
+    validatorMap.put(AUDIENCE, FieldValidator::validateJsonArrayIfPresent);
     validatorMap.put(METADATA, FieldValidator::validateJsonIfPresent);
     validatorMap.put(TAXONOMY, FieldValidator::validateJsonIfPresent);
     validatorMap.put(ORIENTATION, (value) -> (value != null && value instanceof String &&
